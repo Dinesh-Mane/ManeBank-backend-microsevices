ac425ed7fa2cd0a90e58c2fdac3881251f581b8b
Dinesh <dinesh30mane@gmail.com>
2026-09-17 15:34:24 +0530

    Handle all type of exceptions globally

--- accounts/src/main/java/com/dineshmane/accounts/exception/GlobalExceptionHandler.java
+++ accounts/src/main/java/com/dineshmane/accounts/exception/GlobalExceptionHandler.java
@@ -33,4 +33,15 @@ public class GlobalExceptionHandler {
         );
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponseDto);
     }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponseDto> handleGlobalException(Exception exception, WebRequest webRequest){
+        ErrorResponseDto errorResponseDto = new ErrorResponseDto(
+                webRequest.getDescription(false),
+                HttpStatus.INTERNAL_SERVER_ERROR,
+                exception.getMessage(),
+                LocalDateTime.now()
+        );
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponseDto);
+    }
 }
