419aea0f8b067323a71d4f1e650d53dbf5abd691
Dinesh <dinesh30mane@gmail.com>
2026-09-17 14:37:47 +0530

    Implement update account details rest api end-to-end

--- accounts/src/main/java/com/dineshmane/accounts/controller/AccountsController.java
+++ accounts/src/main/java/com/dineshmane/accounts/controller/AccountsController.java
@@ -34,6 +34,25 @@ public class AccountsController {
     @GetMapping("/fetch")
     public ResponseEntity<CustomerDto> fetchAccountDetails(@RequestParam String mobileNumber){
         CustomerDto customerDto = accountService.fetchAccount(mobileNumber);
-        return ResponseEntity.status(HttpStatus.OK).body(customerDto);
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(customerDto);
     }
+
+    @PutMapping("/update")
+    public ResponseEntity<ResponseDto> updateAccountDetails(@RequestBody CustomerDto customerDto){
+
+        boolean isUpdated = accountService.updateAccount(customerDto);
+        if (isUpdated){
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .body(new ResponseDto(AccountsConstants.STATUS_200, AccountsConstants.MESSAGE_200));
+        } else {
+            return ResponseEntity
+                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(new ResponseDto(AccountsConstants.STATUS_500, AccountsConstants.MESSAGE_500));
+        }
+    }
+
+
 }
--- accounts/src/main/java/com/dineshmane/accounts/service/IAccountService.java
+++ accounts/src/main/java/com/dineshmane/accounts/service/IAccountService.java
@@ -5,4 +5,5 @@ import com.dineshmane.accounts.dto.CustomerDto;
 public interface IAccountService {
     void createAccount(CustomerDto customerDto);
     CustomerDto fetchAccount(String mobileNumber);
+    Boolean updateAccount(CustomerDto customerDto);
 }
